46f98ac821b96f78e2e1d0d97599734d47407d79
shivPosted <shivpratap621@gmail.com>
2023-08-19 16:04:10 +0530

    added some winning transition and completed the game 🫡

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ const displayComputerChoice = document.querySelector('.computer-choice img');
 const updatePlayerScore = document.querySelector('.player-display-score');
 const inputValueName = document.querySelector('.input-player-name');
 const updateComputerScore = document.querySelector('.computer-display-score');
-const crownPlayer = document.querySelector('.crown-player');
+const crownPlayer = document.createElement('div');
+crownPlayer.classList.add('crown', 'crown-hidden');
+crownPlayer.textContent = '👑';
+console.log(String(crownPlayer));
 const crownComputer = document.querySelector('.crown-computer');
 
 let playerScore = 0;
@@ -16,26 +19,17 @@ const cpChoice = ['rock', 'paper', 'scissors'];
 
 const nameHandlerLogic = function () {
   playerName.textContent = '';
-  const str = `${inputValueName.value}
-  <div class="crown crown-hidden crown-player">👑</div>`;
-  playerName.insertAdjacentHTML('afterbegin', str);
+  playerName.textContent = inputValueName.value;
+  playerName.append(crownPlayer);
   document.querySelector('.name-pop-up').classList.add('hidden');
   document.querySelector('.overlay').classList.add('hidden');
   document.querySelector('.main').classList.remove('opacity');
 };
 const nameHandlerClick = function (e) {
+  e.preventDefault();
   nameHandlerLogic();
 };
-const nameHandlerEnter = function (e) {
-  e.preventDefault();
-  console.log(e.key);
-  // if (e.key === 'Enter') {
-  //   nameHandlerLogic();
-  // }
-};
 submit.addEventListener('click', nameHandlerClick);
-
-// document.body.addEventListener('keydown', nameHandlerEnter);
 const gameLogic = function (playerChoice, computerChoice) {
   if (
     (playerChoice === 'rock' && computerChoice === 'scissors') ||
@@ -75,22 +69,22 @@ const playFunction = function (e) {
     displayPlayerChoice.src = `img/${chose}.png`;
     displayComputerChoice.src = `img/${cpChoice[rn]}.png`;
     gameLogic(chose, cpChoice[rn]);
-    // if (
-    //   Number(updatePlayerScore.textContent) >
-    //   Number(updateComputerScore.textContent)
-    // ) {
-    //   crownPlayer.classList.remove('crown-hidden');
-    //   crownComputer.classList.add('crown-hidden');
-    // } else if (
-    //   Number(updateComputerScore.textContent) >
-    //   Number(updatePlayerScore.textContent)
-    // ) {
-    //   crownPlayer.classList.add('crown-hidden');
-    //   crownComputer.classList.remove('crown-hidden');
-    // } else {
-    //   crownPlayer.classList.add('crown-hidden');
-    //   crownComputer.classList.add('crown-hidden');
-    // }
+    if (
+      Number(updatePlayerScore.textContent) >
+      Number(updateComputerScore.textContent)
+    ) {
+      crownPlayer.classList.remove('crown-hidden');
+      crownComputer.classList.add('crown-hidden');
+    } else if (
+      Number(updateComputerScore.textContent) >
+      Number(updatePlayerScore.textContent)
+    ) {
+      crownPlayer.classList.add('crown-hidden');
+      crownComputer.classList.remove('crown-hidden');
+    } else {
+      crownPlayer.classList.add('crown-hidden');
+      crownComputer.classList.add('crown-hidden');
+    }
   }, 3000);
 };
 
